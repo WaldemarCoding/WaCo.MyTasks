d38b758abaecb93b9531cfb16108071eaba614f5
WaldemarCoding <17712059+WaldemarCoding@users.noreply.github.com>
2020-04-30 01:26:35 +0200

    Project structure Setup

---
+++ src/WaCo.MyTasks/Logic/WaCo.MyTasks.ToDoModule/ToDoModuleModule.cs
@@ -0,0 +1,20 @@
+﻿using WaCo.MyTasks.ToDoModule.Views;
+using Prism.Ioc;
+using Prism.Modularity;
+using Prism.Regions;
+
+namespace WaCo.MyTasks.ToDoModule
+{
+    public class ToDoModuleModule : IModule
+    {
+        public void OnInitialized(IContainerProvider containerProvider)
+        {
+
+        }
+
+        public void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+
+        }
+    }
+}
